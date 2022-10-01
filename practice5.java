be645403b8d22ba352da06e1d6e5189fbd1855af
Mdfahad2002 <98505797+Mdfahad2002@users.noreply.github.com>
2022-10-01 16:46:13 +0530

    Add files via upload

---
+++ practice5.java
@@ -0,0 +1,11 @@
+package SmartGraduates;
+
+public class practice5 {
+		 public static void main(String[] args) { 
+		 char ch = 'H'; 
+		 int ascii = ch; 
+		int castAscii = (int) ch; 
+		 System.out.println("The ASCII value of " + ch + " is: " + ascii); 
+		 System.out.println("The ASCII value of " + ch + " is: " + castAscii); 
+		 } 
+		}
---
+++ practice6.java
@@ -0,0 +1,32 @@
+package SmartGraduates;
+
+public class practice6 {
+		 public static void main(String[] args) { 
+		 int dividend = 25, divisor = 4; 
+		 
+		 int quotient = dividend / divisor; 
+		 int remainder = dividend % divisor; 
+		 
+		 System.out.println("Quotient = " + quotient); 
+		 System.out.println("Remainder = " + remainder); 
+		 } 
+} 
+		
+//import java.util.Scanner; 
+//		
+//class practice6 
+//		
+//{ 
+//		 public static void main(String args[]) 
+//		 { 
+//		 int num; 
+//		 System.out.println("Enter an Integer number:"); 
+//		 Scanner input = new Scanner(System.in); 
+//		 num = input.nextInt(); 
+//		if ( num % 2 == 0 ) 
+//		 System.out.println("Entered number is even"); 
+//		 else
+//		 System.out.println("Entered number is odd"); 
+//		 } 
+//}
+
