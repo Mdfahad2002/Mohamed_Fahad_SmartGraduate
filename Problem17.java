26a7f3d116e42e2a2ceecd09879a886901ddb137
Mdfahad2002 <98505797+Mdfahad2002@users.noreply.github.com>
2022-10-28 17:54:09 +0530

    Add files via upload

---
+++ Problem17.java
@@ -0,0 +1,16 @@
+package SmartGraduates;
+
+public class Problem17 {
+	Problem17(int val){
+
+    }
+    static int stat=12;
+    public static void main(String[] args) {
+        final int CONST=10;
+        System.out.println(CONST);
+    }
+    void method(){
+        stat=30;
+    }
+}
+
---
+++ Sampleprogram.java
@@ -0,0 +1,11 @@
+package SmartGraduates;
+
+public class Sampleprogram {
+	 public static void main(String[] args) {
+	        int i=-243;
+	        int j=243;
+	        if(i==j)
+	        System.out.println("Number are the same");
+	    }
+	}
+
